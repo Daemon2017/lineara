5353efb65c8270a0337a5c41e466a79262209c50
Daemon2017 <daemon2017@gmail.com>
2026-06-03 03:49:50 +0300

    Правка юникода

--- back/constants.py
+++ back/constants.py
@@ -6,22 +6,21 @@ SIGN_TO_VOWEL = {
 SIGN_TO_SERIES = {
     "P": "DYNAMIC", "T": "DYNAMIC", "K": "DYNAMIC",
     "D": "STATIC", "Q": "STATIC",
-    "M": "SONORANT", "N": "SONORANT",
-    "R": "SONORANT",
-    "S": "SIBILANT"
+    "M": "SONORANT", "N": "SONORANT", "R": "SONORANT",
+    "S": "SIBILANT", "Z": "SIBILANT"
 }
 
 STAGES_RULES = {
     "STRONG": {
         "DYNAMIC": {"P": "pʰ", "T": "tʰ", "K": "kʰ"},
         "STATIC": {"D": "ṭ", "Q": "ḳ"},
-        "SONORANT": {"R": "l"},
-        "SIBILANT": {"S": "s"}
+        "SONORANT": {"M": "m", "N": "n", "R": "l"},
+        "SIBILANT": {"S": "s", "Z": "c"}
     },
     "WEAK": {
         "DYNAMIC": {"P": "b", "T": "d", "K": "g"},
         "STATIC": {"D": "t", "Q": "k"},
-        "SONORANT": {"R": "r"},
-        "SIBILANT": {"S": "z"}
+        "SONORANT": {"M": "m", "N": "n", "R": "r"},
+        "SIBILANT": {"S": "z", "Z": "ʒ"}
     }
 }
